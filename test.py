ed10aea010aff6980ab628dfa4f7b8696789831a
Felipe Printes <felipeprintes@MacBook-Air-de-Felipe.local>
2022-09-19 20:50:00 -0300

    add code in main branch

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print('ola mundo')
 for i in range(10):
     print(i)
     print('print de desenvolvimento')
+    print('Essa foi uma linha de código possível de ser adicionada \
+        direto na branch main')
